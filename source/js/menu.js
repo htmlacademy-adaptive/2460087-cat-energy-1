8a1f72ed8e67419fc165ce06655b36b7cb506d0e
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-06 04:54:26 +0700

    завершает работу над кнопкой

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ const button = document.querySelector('.user-menu__button-menu');
 const nav = document.querySelector('.user-menu');
 
 nav.classList.remove('user-menu--no-js');
+nav.classList.add('user-menu--closed');
 
 button.addEventListener('click', () => {
   nav.classList.toggle('user-menu--closed');
