8553c5d67638b735c2ee235c76817aa9314e685b
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-06 06:25:43 +0700

    добавляет всю работу с меню в медиавыражение, отключает скрипт на ширине больше 797 пикселей

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,16 +1,18 @@
-const button = document.querySelector('.user-menu__button-menu');
-const nav = document.querySelector('.user-menu');
+if (window.innerWidth <= 797) {
+  const button = document.querySelector('.user-menu__button-menu');
+  const nav = document.querySelector('.user-menu');
 
-nav.classList.remove('user-menu--no-js');
-nav.classList.add('user-menu--closed');
+  nav.classList.remove('user-menu--no-js');
+  nav.classList.add('user-menu--closed');
 
-nav.classList.add('user-menu--no-animation');
+  nav.classList.add('user-menu--no-animation');
 
-setTimeout(() => {
-  nav.classList.remove('user-menu--no-animation');
-}, 10);
+  setTimeout(() => {
+    nav.classList.remove('user-menu--no-animation');
+  }, 10);
 
-button.addEventListener('click', () => {
-  nav.classList.toggle('user-menu--closed');
-  nav.classList.toggle('user-menu--opened');
-});
+  button.addEventListener('click', () => {
+    nav.classList.toggle('user-menu--closed');
+    nav.classList.toggle('user-menu--opened');
+  });
+}
