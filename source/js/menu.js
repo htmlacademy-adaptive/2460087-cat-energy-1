0fbb480c27f8120166f4f3674706d525ac122d9b
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-06 05:59:40 +0700

    отключает проигрывание анимации при первой загрузке скрипта на странице

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,6 +4,12 @@ const nav = document.querySelector('.user-menu');
 nav.classList.remove('user-menu--no-js');
 nav.classList.add('user-menu--closed');
 
+nav.classList.add('user-menu--no-animation');
+
+setTimeout(() => {
+  nav.classList.remove('user-menu--no-animation');
+}, 10);
+
 button.addEventListener('click', () => {
   nav.classList.toggle('user-menu--closed');
   nav.classList.toggle('user-menu--opened');
