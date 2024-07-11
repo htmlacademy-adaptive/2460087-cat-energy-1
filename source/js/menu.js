7ceb28140ad02e39b676ff24cb44021669a10ac2
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-11 23:20:24 +0700

    добавляет дисплей блок в меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,18 +1,20 @@
-if (window.innerWidth <= 797) {
+document.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('.user-menu__button-menu');
   const nav = document.querySelector('.user-menu');
 
-  nav.classList.remove('user-menu--no-js');
-  nav.classList.add('user-menu--closed');
+  if (button && nav) {
+    nav.classList.remove('user-menu--no-js');
+    nav.classList.add('user-menu--closed');
 
-  nav.classList.add('user-menu--no-animation');
+    nav.classList.add('user-menu--no-animation');
 
-  setTimeout(() => {
-    nav.classList.remove('user-menu--no-animation');
-  }, 10);
+    setTimeout(() => {
+      nav.classList.remove('user-menu--no-animation');
+    }, 10);
 
-  button.addEventListener('click', () => {
-    nav.classList.toggle('user-menu--closed');
-    nav.classList.toggle('user-menu--opened');
-  });
-}
+    button.addEventListener('click', () => {
+      nav.classList.toggle('user-menu--closed');
+      nav.classList.toggle('user-menu--opened');
+    });
+  }
+});
