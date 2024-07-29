f428adec92ad2d9713882d5ff8d3c4950da95c2b
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-30 01:23:23 +0700

    смещает метку

--- source/js/mapbasics.js
+++ source/js/mapbasics.js
@@ -5,12 +5,17 @@ ymaps.ready(function () {
     [57, 53],
     [113, 106]
   ];
-  const myMapZoom = [13, 14, 15];
 
-  // Определяем начальный центр карты в зависимости от ширины окна
-  let initialCenter = [59.938813148454855, 30.32306884632085];
+  const offsetPlacemark = [
+    [-28.5, -53],
+    [-56.5, -106]
+  ];
+
+  const myMapZoom = [14, 15, 16];
+
+  let initialCenter = [59.938631, 30.323037];
   if (window.innerWidth >= 1100) {
-    initialCenter = [59.938238089377904, 30.30602072589953];
+    initialCenter = [59.938997, 30.313120];
   }
 
   var myMap = new ymaps.Map('map', {
@@ -18,20 +23,14 @@ ymaps.ready(function () {
     zoom: window.innerWidth <= 500 ? myMapZoom[0] : myMapZoom[1]
   });
 
-  // Создаем метку на фиксированных координатах
-  myPlacemark = new ymaps.Placemark([59.938813148454855, 30.32306884632085], {
+  var myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {
     hintContent: 'ул. Большая Конюшенная, д. 19/8',
     balloonContent: '<b>ул. Большая Конюшенная, д. 19/8,</b><br/>' + ' Санкт-Петербург'
   }, {
-    // Опции.
-    // Необходимо указать данный тип макета.
     iconLayout: 'default#image',
-    // Своё изображение иконки метки.
     iconImageHref: '../img/icon/map-pin.svg',
-    // Размеры метки.
     iconImageSize: window.innerWidth <= 500 ? sizePlacemark[0] : sizePlacemark[1],
-    // Смещение левого верхнего угла иконки относительно
-    // её "ножки" (точки привязки).
+    iconImageOffset: window.innerWidth <= 500 ? offsetPlacemark[0] : offsetPlacemark[1]
   });
 
   myMap.geoObjects.add(myPlacemark);
@@ -40,21 +39,22 @@ ymaps.ready(function () {
   myMap.events.add('boundschange', function () {
     if (window.innerWidth <= 500 && check) {
       myPlacemark.options.set('iconImageSize', sizePlacemark[0]);
+      myPlacemark.options.set('iconImageOffset', offsetPlacemark[0]);
       myMap.setZoom(myMapZoom[0], { duration: 300 });
       check = false;
     } else if (window.innerWidth > 500 && !check) {
       myPlacemark.options.set('iconImageSize', sizePlacemark[1]);
+      myPlacemark.options.set('iconImageOffset', offsetPlacemark[1]);
       myMap.setZoom(myMapZoom[1], { duration: 300 });
       check = true;
-    } else if (window.innerWidth > 1200) {
+    } else if (window.innerWidth > 1100) {
       myMap.setZoom(myMapZoom[2], { duration: 300 });
     }
 
-    // Меняем центр карты, если ширина экрана больше или равна 1100 пикселей
     if (window.innerWidth >= 1100) {
-      myMap.setCenter([59.938238089377904, 30.30602072589953]);
+      myMap.setCenter([59.938997, 30.313120]);
     } else {
-      myMap.setCenter([59.938813148454855, 30.32306884632085]);
+      myMap.setCenter([59.938631, 30.323037]);
     }
   });
 });
