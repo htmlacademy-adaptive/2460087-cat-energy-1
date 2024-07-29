baf8c9c507a58dcb58c8db30e24fe324b161f74c
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-30 02:04:25 +0700

    меняет размеры вьюпорта в скрипте

--- source/js/mapbasics.js
+++ source/js/mapbasics.js
@@ -22,29 +22,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var myMap = new ymaps.Map('map', {
       center: initialCenter,
-      zoom: window.innerWidth <= 500 ? myMapZoom[0] : myMapZoom[1]
+      zoom: window.innerWidth <= 600 ? myMapZoom[0] : myMapZoom[1]
     });
 
     var myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {
       hintContent: 'ул. Большая Конюшенная, д. 19/8',
-      balloonContent: '<b>ул. Большая Конюшенная, д. 19/8,</b><br/>' + ' Санкт-Петербург'
+      balloonContent: '<b>ул. Большая Конюшенная, д. 19/8,</b></br>' + ' Санкт-Петербург'
     }, {
       iconLayout: 'default#image',
       iconImageHref: '../img/map/map-pin.svg',
-      iconImageSize: window.innerWidth <= 500 ? sizePlacemark[0] : sizePlacemark[1],
-      iconImageOffset: window.innerWidth <= 500 ? offsetPlacemark[0] : offsetPlacemark[1]
+      iconImageSize: window.innerWidth <= 600 ? sizePlacemark[0] : sizePlacemark[1],
+      iconImageOffset: window.innerWidth <= 600 ? offsetPlacemark[0] : offsetPlacemark[1]
     });
 
     myMap.geoObjects.add(myPlacemark);
 
     let check = true;
     myMap.events.add('boundschange', function () {
-      if (window.innerWidth <= 500 && check) {
+      if (window.innerWidth <= 600 && check) {
         myPlacemark.options.set('iconImageSize', sizePlacemark[0]);
         myPlacemark.options.set('iconImageOffset', offsetPlacemark[0]);
         myMap.setZoom(myMapZoom[0], { duration: 300 });
         check = false;
-      } else if (window.innerWidth > 500 && !check) {
+      } else if (window.innerWidth > 600 && !check) {
         myPlacemark.options.set('iconImageSize', sizePlacemark[1]);
         myPlacemark.options.set('iconImageOffset', offsetPlacemark[1]);
         myMap.setZoom(myMapZoom[1], { duration: 300 });
