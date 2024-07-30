d23cc2dcd0b7787d9bd76ded059657c5eaf32697
LevchenkoLyubov <edeven.gothery@gmail.com>
2024-07-30 08:47:20 +0700

    скрывает изображение в слайдере чтобы не было оторванной попки кота

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('.slider__container');
 const slider = document.querySelector('.slider__input');
+const beforeImage = document.querySelector('.slider__image--before');
+
 document.addEventListener('input', (e) => {
-  container.style.setProperty('--position', slider.value + '%');
-})
+  const position = slider.value + '%';
+  container.style.setProperty('--position', position);
+
+  if (slider.value <= 38) {
+    beforeImage.classList.add('hidden');
+  } else {
+    beforeImage.classList.remove('hidden');
+  }
+});
