5705e836638d4e4f82ed35490fc877f9c88257c8
Keks <keks@htmlacademy.ru>
2024-08-13 19:41:16 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/menu.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.user-menu__button-menu');
+  const nav = document.querySelector('.user-menu');
+  const navList = nav.querySelector('.user-menu__navigation-list');
+
+  if (button && nav && navList) {
+    nav.classList.remove('user-menu--no-js');
+    nav.classList.add('user-menu--closed');
+
+    nav.classList.add('user-menu--no-animation');
+
+    setTimeout(() => {
+      nav.classList.remove('user-menu--no-animation');
+    }, 10);
+
+    button.addEventListener('click', () => {
+      nav.classList.toggle('user-menu--opened');
+      nav.classList.toggle('user-menu--closed');
+    });
+  }
+});
